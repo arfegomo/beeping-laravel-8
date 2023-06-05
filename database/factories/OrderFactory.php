7e064d490fa5860159b0e33724152b734979bb67
arfegomo <ariel.fernando.gonzalez.moreno@gmail.com>
2023-06-05 19:14:35 +0200

    Carga de registros a la DB

---
+++ database/factories/OrderFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Order;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class OrderFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'order_ref' => rand(2000,10000),
+            'customer_name' => $this->faker->name(),
+        ];
+    }
+}
